92bfe4f263433b3de53b9946a0a873c0bfca57f3
pobi <pobittrachandraroy2001@gmail.com>
2024-05-07 11:50:14 +0600

    intitalize tsconfig file and setup this project also write about topics on typestripts in intro.ts

---
+++ dist/intro.js
@@ -0,0 +1,84 @@
+"use strict";
+{
+    /*
+    //in this project i will revise my concepts about typescript. like----
+
+    1. Basics data types
+
+        1. primitive data types
+            1. number,
+            2. string,
+            3. boolean => true or false,
+            4. null,
+            5. undefined,
+            6. symbol,
+            7............
+
+        2.non-primitive data types
+            1. array,
+            2. tupple,
+            3. object,
+            4............
+
+
+
+    2. optional and literal string.
+    3. function in typescript.
+    4. function in object (method).
+    5. spread and rest operator.
+    6. destructuring.
+    7. type alias in typescript.
+
+    8. some types in typescript---
+        1. union type,
+        2. intersection type,
+        3. never type.
+        4. unknown type.
+        5. nullable type.
+        6. generic type.
+        7. conditional type.
+        8. mapped type.
+        
+        9. utilities types---
+            1. pick type.
+            2. omit type.
+            3. required type.
+            4. readonly type.
+            5. record type.
+            6. partial type.
+
+    9. ternary in typescript.
+    10. optional chaining.
+    11. nullish coalescing.
+    12. type assertion or type narrowing.
+    13. interface, interface vs type.
+    14. constraints.
+    15. asynchronous in typescript.
+    16. promise in typescript.
+
+    17. about OOP in typescript--- (4 concepts)---
+        1. inheritance.
+        2. polymorphism.
+        3. abstraction.
+        4. encapsulation.
+
+    18. class and objects in typescript.
+    19. type guard with---
+        1. typeof.
+        2. in.
+        3. instance of.
+
+    20. access modifiers---
+        1. readonly.
+        2. private.
+        3. protected.
+        4. public.
+
+    21. getter and setter.
+    22. statics in OOP.
+
+
+    {---------ends here------------}
+    writen by me (pobittra roy)
+    */
+}
