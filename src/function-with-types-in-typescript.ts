3753b555d3a08ae3794310f62c05455ea46682ac
pobi <pobittrachandraroy2001@gmail.com>
2024-05-07 12:41:19 +0600

    revise about function with type in typescript

---
+++ src/function-with-types-in-typescript.ts
@@ -0,0 +1,19 @@
+{
+  //function with type in typescripts
+  {
+    //normal function
+    function addition(numOne: number, numTwo: number): number {
+      return numOne + numTwo;
+    }
+    const resultOfAddition = addition(12, 5);
+    console.log(resultOfAddition);
+  }
+  {
+    //arrow function
+    const substraction = (numOne: number, numTwo: number): number =>
+      numOne - numTwo;
+
+    const resultOfSubtraction  = substraction(12,5);
+    console.log(resultOfSubtraction);
+  }
+}
